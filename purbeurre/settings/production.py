bb8e8f8931488cdc40d645bce0f59f6f4a409a73
snakejj <snakejj@github.com>
2021-01-12 19:00:07 +0100

    Fixing Allowed host var

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -19,7 +19,7 @@ sentry_sdk.init(
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG_PROD")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['159.65.89.33','localhost']
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
