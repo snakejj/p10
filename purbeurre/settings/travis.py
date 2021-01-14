0c1cf35e9a4c69689031084b0df068008a0a06d6
snakejj <snakejj@github.com>
2021-01-14 12:56:17 +0100

    Adding 'postgresql' as a service

--- purbeurre/settings/travis.py
+++ purbeurre/settings/travis.py
@@ -2,7 +2,7 @@ from . import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': '',
         'USER': 'postgres',
         'PASSWORD': '',
